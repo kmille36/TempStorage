45639746d1decd08ad3817b570a038935527801f
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-03-25 13:17:08 +0700

    Create ngrokchoice.bat

---
+++ ngrokchoice.bat
@@ -0,0 +1,17 @@
+@echo off
+curl -LkO https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip
+tar xf ngrok-stable-windows-amd64.zip
+set /p id="Paste NGROK AUTHTOKEN: "
+echo "======================="
+echo "choose ngrok region (for better connection)."
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+set /p re="Choose ngrok region: "
+ngrok authtoken %id%
+ngrok tcp --region %re% 3389
