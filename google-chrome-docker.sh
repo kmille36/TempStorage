c92ee695666dd67b6d14f1a62b17161e51d20c96
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-07-28 01:18:11 +0700

    Update google-chrome-docker.sh

--- google-chrome-docker.sh
+++ google-chrome-docker.sh
@@ -2,20 +2,8 @@ clear
 echo "Script by fb.com/thuong.hai.581"
 echo "Support: Ubuntu/Centos/Debian"
 echo "Finding your linux distro"
-dist=`grep ID /etc/*-release | awk -F '=' '{print $2}'`
-if [ $dist = "centos" ] ; then
-	echo "Your distro is CentOS"
-	sleep 1
-        echo "Updating your system"
-        sudo yum update -y
-        yum install wget -y
-        clear
-        echo "Installing google-chrome"
-        wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-        sudo yum install -y ./https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-
-elif [ $dist = "Ubuntu" -o $dist = "debian" ] ; then
-      	echo "Your distro is Ubuntu"
+if command -v apt-get >/dev/null; then
+      	echo "Your distro is Ubuntu or Debian"
       	sleep 1
         echo "Updating your system"
        	sudo apt-get update -y
@@ -24,8 +12,20 @@ elif [ $dist = "Ubuntu" -o $dist = "debian" ] ; then
         echo "Installing google-chrome"
         wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
         sudo apt install -y ./google-chrome-stable_current_amd64.deb
-
-        
+elif command -v yum >/dev/null; then
+  	echo "Your distro is CentOS"
+	sleep 1
+        echo "Updating your system"
+        sudo yum update -y
+        yum install wget -y
+        clear
+        echo "Installing google-chrome"
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+        sudo yum install -y ./https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+else
+  echo "Not Supported OS !!!"
 fi
 echo "Lauching Google-Chrome"
 google-chrome --no-sandbox
+
+
