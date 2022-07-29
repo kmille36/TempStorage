00d37bf58f221bcd36f1dcaedff40e9c3eb17fe9
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-07-29 18:34:44 +0700

    Update post-script.sh

--- post-script.sh
+++ post-script.sh
@@ -3,4 +3,4 @@
     curl -k -L -O https://github.com/kmille36/TempStorage/raw/main/guacamole.z01 
     zip -F guacamole.zip --out file-large.zip 
     unzip file-large.zip  
-    docker run --network host -d -v /root/guacamole:/config oznu/guacamole
+    docker run --network host -d -v /root/guacamole:/config jwetzell/guacamole
