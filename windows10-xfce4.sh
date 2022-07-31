22db9d46070244426b84c555f42ef51072a0d96d
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-07-31 22:16:54 +0700

    Create windows10-xfce4.sh

---
+++ windows10-xfce4.sh
@@ -0,0 +1,9 @@
+clear
+echo "Script by fb.com/thuong.hai.581"
+echo "Transform any xfce4 into windows 10"
+echo "Install neeeded packages"
+sudo apt update
+sudo wget http://archive.kali.org/kali/pool/main/k/kali-undercover/kali-undercover_2021.4.0_all.deb 
+sudo apt install ./kali-undercover_2021.4.0_all.deb -y
+echo "Applying windows 10 theme"
+kali-undercover
