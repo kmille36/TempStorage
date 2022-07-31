63d90ea542ef68baeb6267b1d29a05bb5513fea8
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-07-31 21:11:44 +0700

    Update post-script.sh

--- post-script.sh
+++ post-script.sh
@@ -4,3 +4,4 @@
     zip -F guacamole.zip --out file-large.zip 
     unzip file-large.zip  
     docker run --network host -d -v /root/guacamole:/config jwetzell/guacamole
+    sudo fallocate -l 1G /swapfile ; sudo chmod 600 /swapfile ; sudo mkswap /swapfile ; sudo swapon /swapfile
