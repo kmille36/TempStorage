275972af8a258cac31414eeb033cf6e37d66c151
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-08-06 01:27:33 +0700

    Delete post-script.sh

--- post-script.sh
+++
@@ -1,7 +0,0 @@
-    apt install zip -y 
-    curl -k -L -O https://github.com/kmille36/TempStorage/raw/main/guacamole.zip 
-    curl -k -L -O https://github.com/kmille36/TempStorage/raw/main/guacamole.z01 
-    zip -F guacamole.zip --out file-large.zip 
-    unzip file-large.zip  
-    docker run --network host -d -v /root/guacamole:/config jwetzell/guacamole
-    sudo fallocate -l 1G /swapfile ; sudo chmod 600 /swapfile ; sudo mkswap /swapfile ; sudo swapon /swapfile
