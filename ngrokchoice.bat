0d30d8b3e2e33bfd6d42a66dfc62dc0f2bd31233
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-09-18 21:56:51 +0700

    Update ngrokchoice.bat

--- ngrokchoice.bat
+++ ngrokchoice.bat
@@ -1,4 +1,6 @@
 @echo off
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 sc start audiosrv
 sc config Audiosrv start= auto
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
