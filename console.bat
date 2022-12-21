f9c776e3b2b1118f2a7ef90a3a1a8738cb439dc3
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-12-22 00:46:29 +0700

    Create console.bat

---
+++ console.bat
@@ -0,0 +1,11 @@
+
+@echo off
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+title Install 
+
+for /f "skip=1 tokens=3" %%s in ('query user %USERNAME%') do (
+  %windir%\System32\tscon.exe %%s /dest:console
+)
+
+exit
