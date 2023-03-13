a7c338014e4770e72746d4a27bb9e8a1d5b35c7c
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2023-03-13 23:00:22 +0700

    Update novnc-audio-user.sh

--- novnc-audio-user.sh
+++ novnc-audio-user.sh
@@ -12,10 +12,10 @@ BLUE='\033[0;34m'
 
 echo "--------------------------------------------------------"
 echo -e "${BLUE}Free Ubuntu 22.04 VM by fb.com/thuong.hai.581"
-echo -e "${BLUE}Every VM valid for 1 hour. Reload page to get new one${NC}"
+echo -e "${BLUE}Every VM valid for 2 hours. Reload page to get new one${NC}"
 echo "--------------------------------------------------------"
 echo -e "${RED}Ubuntu 22.04 NoVNC${NC} READY. Password is ${RED}$passwd${NC} . Go to: ${RED}http:///$ip:$number${NC}"
 echo "-------------------------------------------------------------------------------"
 
-timeout 3600 docker run --rm --privileged --shm-size 1g -p $number:10000 -e VNC_PASSWD=$passwd -e PORT=10000 -e AUDIO_PORT=1699 -e WEBSOCKIFY_PORT=6900 -e VNC_PORT=5900 -e SCREEN_WIDTH=1024 -e SCREEN_HEIGHT=768 -e SCREEN_DEPTH=24 thuonghai2711/ubuntu-novnc-pulseaudio:22.04
+timeout 7200 docker run --rm --privileged --shm-size 1g -p $number:10000 -e VNC_PASSWD=$passwd -e PORT=10000 -e AUDIO_PORT=1699 -e WEBSOCKIFY_PORT=6900 -e VNC_PORT=5900 -e SCREEN_WIDTH=1024 -e SCREEN_HEIGHT=768 -e SCREEN_DEPTH=24 thuonghai2711/ubuntu-novnc-pulseaudio:22.04
 
