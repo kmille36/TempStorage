ff0279b35f2ca5156730ad7d4f5679f6b605e4f0
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2024-02-04 20:22:54 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,17 +11,17 @@ sudo mkdir -pm755 /dev/input
 sudo touch /dev/input/{js0,js1,js2,js3}
 
 # Start a virtual X11 server, skip this line if an X server already exists or you are already using a display
-# Xvfb -screen :0 8192x4096x24 +extension RANDR +extension GLX +extension MIT-SHM -nolisten tcp -noreset -shmem 2>&1 >/tmp/Xvfb.log &
+Xvfb -screen :0 8192x4096x24 +extension RANDR +extension GLX +extension MIT-SHM -nolisten tcp -noreset -shmem 2>&1 >/tmp/Xvfb.log &
 # Ensure the X server is ready
-# until [[ -S /tmp/.X11-unix/X0 ]]; do sleep 1; done && echo 'X Server is ready'
+until [[ -S /tmp/.X11-unix/X0 ]]; do sleep 1; done && echo 'X Server is ready'
 # Initialize PulseAudio (set PULSE_SERVER to unix:/run/pulse/native if your user is in the pulse-access group), omit the below lines if a PulseAudio server is already running
-# export PULSE_SERVER=tcp:127.0.0.1:4713
-# sudo /usr/bin/pulseaudio -k >/dev/null 2>&1
-# sudo /usr/bin/pulseaudio --daemonize --system --verbose --log-target=file:/tmp/pulseaudio.log --realtime=true --disallow-exit -L 'module-native-protocol-tcp auth-ip-acl=127.0.0.0/8 port=4713 auth-anonymous=1'
+export PULSE_SERVER=tcp:127.0.0.1:4713
+sudo /usr/bin/pulseaudio -k >/dev/null 2>&1
+sudo /usr/bin/pulseaudio --daemonize --system --verbose --log-target=file:/tmp/pulseaudio.log --realtime=true --disallow-exit -L 'module-native-protocol-tcp auth-ip-acl=127.0.0.0/8 port=4713 auth-anonymous=1'
 # Replace this line with your desktop environment session or skip this line if already running, use VirtualGL `vglrun` here if needed
-# [[ "${START_XFCE4:-true}" == "true" ]] && rm -rf ~/.config/xfce4 && xfce4-session &
+[[ "${START_XFCE4:-true}" == "true" ]] && rm -rf ~/.config/xfce4 && xfce4-session &
 # Replace to your resolution if using without resize, skip if there is a physical display
-# selkies-gstreamer-resize 1280x720
+selkies-gstreamer-resize 1280x720
 
 # Choose your video encoder, change to x264enc for software encoding or other encoders for different hardware
 # Do not set enable_resize to true if there is a physical display
